d934752c83dc9d2c98fde648a1ab258f4d859e0e
sergeyvakhrin <parlamentsv@mail.ru>
2024-07-26 21:26:32 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+from config import EMPLOYEE_PATH
 from src.classes.api_request import HH
-
-api_request = HH('python')
-
-employee = api_request.get_api()
-
+from src.classes.empworker import EmpWorker
+from src.classes.fileworker import FileWorker
+from src.classes.vacworker import VacWorker
 
 
+def main():
+
+    # Запрос Работодателей
+    api_request = HH()
+
+    # Получение списка Работодалетей
+    employers: list = api_request.get_api('https://api.hh.ru/employers/', 'python')
+
+    # Сохранение в .json
+    fileworker = FileWorker(employers, EMPLOYEE_PATH)
+    fileworker.save_data()
+
+    # Получение списка экземпляров класса Работодателей
+    emp_worker = EmpWorker()
+    employer_list: list = emp_worker.get_employres(employers)
+
+    # Получение списка экземлпров класса Вакансий всех работодателей с id работодателя
+    vac_worker = VacWorker()
+    vacancies_list: list = []
+    for employee in employer_list:
+        vacancies: list = api_request.get_api(employee.vacancies_url)
+        emp_vacancies: list = vac_worker.get_vacancies(employee, vacancies)
+        vacancies_list.extend(emp_vacancies)
 
 
-print(len(employee))
+
+
+
+
+if __name__ == "__main__":
+    main()
