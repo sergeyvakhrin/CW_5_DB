0d1dd16490aa1cbb7d2615dafb2444cc49b0fb22
sergeyvakhrin <parlamentsv@mail.ru>
2024-07-26 21:22:03 +0300

    fix class APIRequests.py

--- src/classes/api_request.py
+++ src/classes/api_request.py
@@ -7,21 +7,21 @@ class APIRequests(ABC):
     """ Абстрактный метод для наследования классами запросов """
 
     @abstractmethod
-    def get_api(self):
+    def get_api(self, url, keyword=""):
         pass
 
 
 class HH(APIRequests):
     """ Класс для API запроса на HH.ru """
-    def __init__(self, url, keyword=""):
-        self.url = url
+    def __init__(self):
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {'text': '', 'page': 0, 'per_page': 100, 'only_with_vacancies': False, 'sort_by': 'by_name'}
         self.employers_list = []
-        self.params['text'] = keyword
 
-    def get_api(self):
+    def get_api(self, url, keyword="") -> list[dict]:
         """ Метод для отправки API запроса на HH.ru"""
+        self.url = url
+        self.params['text'] = keyword
         while self.params['page'] != 20:
             response = requests.get(url=self.url, headers=self.headers, params=self.params)
             employers = response.json()['items']
