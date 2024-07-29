5e73e11b662c660103ab988eba5af35e164cd10b
sergeyvakhrin <parlamentsv@mail.ru>
2024-07-29 13:52:04 +0300

    addition main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import json
-
 from config import EMPLOYEE_PATH, VACANCIES_PATH
 from src.classes.api_request import HH
+from src.classes.dbworker import DataBaseWorker
 from src.classes.empworker import EmpWorker
 from src.classes.fileworker import FileWorker
 from src.classes.vacworker import VacWorker
+from src.utils.config import config
 
 
 def main():
 
-    # Запрос Работодателей
-    emp_api_request = HH()
+    # Создание Таблиц в базе данных
+    param = config()
+    db_worker = DataBaseWorker(**param)
+
+    db_worker.create_table()
+
 
     # Получение списка Работодалетей
-    employers: list = emp_api_request.get_api('https://api.hh.ru/employers/', 'водитель')
-
+    emp_api_request = HH('https://api.hh.ru/employers/', 'java')
+    employers: list = emp_api_request.get_api()
     # Сохранение в .json
     emp_fileworker = FileWorker()
     emp_fileworker.save_data(employers, EMPLOYEE_PATH, 'wt')
@@ -23,19 +27,38 @@ def main():
     emp_worker = EmpWorker()
     employer_list: list = emp_worker.get_employres(employers)
 
-    # Получение списка экземлпров класса Вакансий всех работодателей с id работодателя
-    vac_api_request = HH()
+
+    # Получение списка экземлпров класса Вакансий всех работодателей
+
     vac_fileworker = FileWorker()
-    vac_worker = VacWorker()
+    # vac_worker = VacWorker()
 
-    vacancies_list: list = []
+    list_to_save_json_vacancy = []
     for employee in employer_list:
-        vacancies: list = vac_api_request.get_api(employee.vacancies_url)
 
-        vac_fileworker.save_data(vacancies, VACANCIES_PATH, 'wt')
+        vac_api_request = HH(employee.vacancies_url)
+        vacancies = vac_api_request.get_api()
 
-        emp_vacancies: list = vac_worker.get_vacancies(employee, vacancies)
-        vacancies_list.extend(emp_vacancies)
+        list_to_save_json_vacancy.extend(vacancies)
+
+        # Получение списка экземпляров класса Вакансий
+        emp_vacancies: list = VacWorker.get_vacancies(employee, vacancies)
+        employee.vacancies = emp_vacancies
+
+    # Сохранение в .json
+    vac_fileworker.save_data(list_to_save_json_vacancy, VACANCIES_PATH, 'wt')
+
+    db_worker.insert_table_emp(employer_list)
+    db_worker.insert_table_vac(employer_list)
+
+
+
+
+
+
+
+
+
 
 
 
