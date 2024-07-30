38d49732be8c6680fb3f9a3b136eb0049c77ada2
sergeyvakhrin <parlamentsv@mail.ru>
2024-07-30 21:35:39 +0300

    add methods class dbmanager

--- src/classes/dbmanager.py
+++ src/classes/dbmanager.py
@@ -8,29 +8,45 @@ class DBManager:
         """ Конструктор класса """
         self.conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
 
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> list[tuple]:
         """ Получает список всех компаний и количество вакансий у каждой компании """
         cur = self.conn.cursor()
-        cur.execute("""
-                           
-                                   """)
+        cur.execute("SELECT name, open_vacancies FROM employee")
         res = cur.fetchall()
-        self.conn.commit()
+        cur.close()
         return res
 
     def get_all_vacancies(self):
         """ Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты
             и ссылки на вакансию """
-        pass
+        cur = self.conn.cursor()
+        cur.execute("SELECT vacancy.name AS vacancy, employee.name AS employer, salary_from, vacancy.url  FROM "
+                    "vacancy LEFT JOIN employee USING(employer_id)")
+        res = cur.fetchall()
+        cur.close()
+        return res
 
     def get_avg_salary(self):
         """ Получает среднюю зарплату по вакансиям """
-        pass
+        cur = self.conn.cursor()
+        cur.execute("SELECT AVG(salary_from) FROM vacancy")
+        res = cur.fetchall()
+        cur.close()
+        return res
 
     def get_vacancies_with_higher_salary(self):
         """ Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям """
-        pass
+        cur = self.conn.cursor()
+        cur.execute("SELECT name, salary_from FROM vacancy WHERE salary_from > (SELECT AVG(salary_from) FROM vacancy)")
+        res = cur.fetchall()
+        cur.close()
+        return res
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, keyword=''):
         """ Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python"""
-        pass
+        cur = self.conn.cursor()
+        cur.execute(f"SELECT name FROM vacancy WHERE name LIKE '%{keyword}%'")
+        res = cur.fetchall()
+        cur.close()
+        return res
+
